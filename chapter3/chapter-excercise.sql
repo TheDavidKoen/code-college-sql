c5149bbaa8aaeddf76be62d227c53ef95390cee7
David Koen <davidodawie@gmail.com>
2024-07-31 18:40:27 +0200

    published chapter 3.

---
+++ chapter3/chapter-excercise.sql
@@ -0,0 +1,7 @@
+/* smallint will be the answer for question 1, because that number data type takes numbers from −32768 to +32767, and no driver is 
+expected to exceed +999. */
+
+/* text will be a good data type for first and last names, because you can never predict how short or long individuals'
+name could be. */
+
+/* I get a syntax error because the given format does not match the timestamp data type format. */
