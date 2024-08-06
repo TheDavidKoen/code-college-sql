d0f201dde20d68fe253d8bb2269c9ab829a8dd6b
David Koen <davidodawie@gmail.com>
2024-08-06 15:39:48 +0200

    updated chapter 8

--- chapter8/chapter-excercises.sql
+++ chapter8/chapter-excercises.sql
@@ -82,22 +82,6 @@ COPY pls_fy2014_pupld14a
 FROM 'C:\01-my-folder\Work\code-college\my_code\software\sql\chapter8\pls_fy2014_pupld14a.csv'
 WITH (FORMAT CSV, HEADER);
 
-SELECT * FROM pls_fy2014_pupld14a;
-
-SELECT count(*)
-FROM pls_fy2014_pupld14a;
-
-SELECT count(salaries)
-FROM pls_fy2014_pupld14a;
-
-SELECT max(visits), min(visits)
-FROM pls_fy2014_pupld14a;
-
-SELECT stabr
-FROM pls_fy2014_pupld14a
-GROUP BY stabr
-ORDER BY stabr;
-
 CREATE TABLE pls_fy2009_pupld09a (
     stabr varchar(2) NOT NULL,
     fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
@@ -178,7 +162,97 @@ COPY pls_fy2009_pupld09a
 FROM 'C:\01-my-folder\Work\code-college\my_code\software\sql\chapter8\pls_fy2009_pupld09a.csv'
 WITH (FORMAT CSV, HEADER);
 
-SELECT * FROM pls_fy2009_pupld09a;
+SELECT count(*)
+FROM pls_fy2014_pupld14a;
 
 SELECT count(*)
 FROM pls_fy2009_pupld09a;
+
+SELECT count(salaries)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(libname)
+FROM pls_fy2014_pupld14a;
+
+SELECT count(DISTINCT libname)
+FROM pls_fy2014_pupld14a;
+
+SELECT libname, count(libname)
+FROM pls_fy2014_pupld14a
+GROUP BY libname
+ORDER BY count(libname) DESC;
+
+SELECT libname, city, stabr
+FROM pls_fy2014_pupld14a
+WHERE libname = 'OXFORD PUBLIC LIBRARY';
+
+SELECT max(visits), min(visits)
+FROM pls_fy2014_pupld14a;
+
+SELECT stabr
+FROM pls_fy2014_pupld14a
+GROUP BY stabr
+ORDER BY stabr;
+
+SELECT stabr
+FROM pls_fy2009_pupld09a
+GROUP BY stabr
+ORDER BY stabr;
+
+SELECT city, stabr
+FROM pls_fy2014_pupld14a
+GROUP BY city, stabr
+ORDER BY city, stabr;
+
+SELECT city, stabr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY city, stabr
+ORDER BY count(*) DESC;
+
+
+SELECT stabr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY stabr
+ORDER BY count(*) DESC;
+
+SELECT stabr, stataddr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY stabr, stataddr
+ORDER BY stabr ASC, count(*) DESC;
+
+SELECT sum(visits) AS visits_2014
+FROM pls_fy2014_pupld14a
+WHERE visits >= 0;
+
+SELECT sum(visits) AS visits_2009
+FROM pls_fy2009_pupld09a
+WHERE visits >= 0;
+
+SELECT sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0;
+
+SELECT pls14.stabr,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+ORDER BY pct_change DESC;
+
+SELECT pls14.stabr,
+       sum(pls14.visits) AS visits_2014,
+       sum(pls09.visits) AS visits_2009,
+       round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
+                    sum(pls09.visits) * 100, 2 ) AS pct_change
+FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+ON pls14.fscskey = pls09.fscskey
+WHERE pls14.visits >= 0 AND pls09.visits >= 0
+GROUP BY pls14.stabr
+HAVING sum(pls14.visits) > 50000000
+ORDER BY pct_change DESC;
